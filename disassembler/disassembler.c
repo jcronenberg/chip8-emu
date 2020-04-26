6027472cb49ac89b430672bccd83e667633a2feb
Jorik Cronenberg <jcronenberg@suse.de>
2020-04-26 18:09:25 +0200

    Expand disassembler

--- disassembler/disassembler.c
+++ disassembler/disassembler.c
@@ -66,20 +66,29 @@ int disassemble8080Op(unsigned char *codebuffer, int pc)
     switch (*code)
     {
         case 0x00: printf("NOP"); break;
-        case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+        case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes = 3; break;
         case 0x02: printf("STAX   B"); break;
         case 0x03: printf("INX    B"); break;
         case 0x04: printf("INR    B"); break;
         case 0x05: printf("DCR    B"); break;
-        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;
+        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes = 2; break;
         case 0x07: printf("RLC"); break;
         case 0x08: printf("NOP"); break;
+        case 0x09: printf("DAD    B"); break;
+        case 0x0a: printf("LDAX   B"); break;
+        case 0x0b: printf("DCX    B"); break;
+        case 0x0c: printf("INR    C"); break;
+        case 0x0d: printf("DCR    C"); break;
+        case 0x0e: printf("MVI    C,#$%02x", code[1]); opbytes = 2; break;
+        case 0x0f: printf("RRC"); break;
+        case 0x10: printf("NOP"); break;
+        case 0x11: printf("LXI    D,#$%02x%02x", code[1], code[2]); opbytes = 3; break;
         /* ........ */
         case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;
         /* ........ */
-        case 0xc3: printf("JMP    $%02x%02x",code[2],code[1]); opbytes = 3; break;
+        case 0xc3: printf("JMP    $%02x%02x", code[2], code[1]); opbytes = 3; break;
         /* ........ */
-	default: printf("Unknown opteration"); break;
+	default: printf("Unknown operation"); break;
     }
 
     printf("\n");
